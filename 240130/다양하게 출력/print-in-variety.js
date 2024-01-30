599eb4191f64e9aa6678ae12a41d46330408d8df
YounseokO <104410211+YounseokO@users.noreply.github.com>
2024-01-30 12:54:10 +0900

    Title: 다양하게 출력, Time: 173ms, Memory: 14MB, Status: Passed - CodeTree

--- 240130/다양하게 출력/print-in-variety.js
+++ 240130/다양하게 출력/print-in-variety.js
@@ -1,8 +1,8 @@
-const stringEx1 = "Total days in Year"
-const stringEx2 = 365
-const stringEx3 = "Circumference rater"
-const stringEx4 = 3.1415926535
-console.log(stringEx1)
-console.log(stringEx2)
-console.log(stringEx3)
-console.log(stringEx4)
+const stringEx1 = "Total days in Year";
+const stringEx2 = "365";
+const stringEx3 = "Circumference rate";
+const stringEx4 = "3.1415926535";
+console.log(stringEx1);
+console.log(stringEx2);
+console.log(stringEx3);
+console.log(stringEx4);
