ecf59a37ae1a28a2874fc8d26e37949e6db5af13
YounseokO <104410211+YounseokO@users.noreply.github.com>
2024-01-31 10:17:38 +0900

    Title: 3 또는 5의 배수, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240131/3 또는 5의 배수/multiples-of-3-or-5.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0);
+let a = Number(input);
+
+if(a%3 == 0){
+    console.log("YES");
+}else{
+    console.log("No");
+}
+if(a%5 == 0){
+    console.log("YES");
+}else{
+    console.log("No");
+}
