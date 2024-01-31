25f21b5f24c27573f4a9a48d7eb12be516e3c985
YounseokO <104410211+YounseokO@users.noreply.github.com>
2024-01-31 10:14:32 +0900

    Title: 두 번의 연산, Time: 177ms, Memory: 14MB, Status: Passed - CodeTree

--- 240131/두 번의 연산/two-operations.js
+++ 240131/두 번의 연산/two-operations.js
@@ -3,8 +3,8 @@ let input = fs.readFileSync(0);
 input = Number(input);
 if(input%2 == 1){
     input += 3;
-    if(input%3 == 0){
-        input /= 3;
-    }
+}
+if(input%3 == 0){
+    input /= 3;
 }
 console.log(input)
