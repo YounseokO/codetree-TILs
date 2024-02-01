71ca1d1d18bd6a74d610f16e9065c4b4b095dd94
YounseokO <104410211+YounseokO@users.noreply.github.com>
2024-02-01 10:24:23 +0900

    Title: 진법 변환 6, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240201/진법 변환 6/base-conversion-6.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString();
+
+input = input.split(/\s/);
+
+let A = Number(input[0]);
+let B = Number(input[2]);
+let N = input[1];
+
+let val = 0;
+
+if(A == 10){
+    //10진수 -> N진수
+    val = N.toString(B);
+    console.log(val)
+}else if(B == 10){
+    //N진수 -> 10진수
+    val = parseInt(N , A);
+    console.log(val)
+}else{
+    //N진수 -> N진수
+    val = parseInt(N , A).toString(B);
+    console.log(val)
+}
