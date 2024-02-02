c78b0fa39f499a538602d5ce349babd83acbff83
YounseokO <104410211+YounseokO@users.noreply.github.com>
2024-02-02 13:46:33 +0900

    Title: 짝수만 출력, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240202/짝수만 출력/output-only-even.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString();
+
+input = input.split(/\s/);
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let sum = "";
+
+while(a <= b){
+    if(a%2 == 0){
+        sum += a + " ";
+    }
+    a++;
+}
+
+console.log(sum);
