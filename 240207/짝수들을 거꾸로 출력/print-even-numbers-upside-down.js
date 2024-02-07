1e2a21521da5820552221b34b81b26e627ab7647
YounseokO <104410211+YounseokO@users.noreply.github.com>
2024-02-07 12:52:27 +0900

    Title: 짝수들을 거꾸로 출력, Time: 237ms, Memory: 14MB, Status: Passed - CodeTree

--- 240207/짝수들을 거꾸로 출력/print-even-numbers-upside-down.js
+++ 240207/짝수들을 거꾸로 출력/print-even-numbers-upside-down.js
@@ -4,12 +4,19 @@ let input = fs.readFileSync(0).toString();
 input = input.split(/\n/);
 let n = Number(input[0]);
 let intNum = input[1].split(/\s/);
-intNum = intNum.reverse();
+//intNum = intNum.reverse();
 
 let val = "";
+for(var i = n-1; i >= 0; i--){
+    if(Number(intNum[i])%2 == 0){
+        val += intNum[i] + " ";
+    }
+}
+/*
 for(var i = 0; i < n; i++){
     if(Number(intNum[i])%2 == 0){
         val += intNum[i] + " ";
     }
 }
+*/
 console.log(val);
