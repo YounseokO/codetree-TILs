cb4cdca314976d6beb9e4256e45aa573d3af4c76
YounseokO <104410211+YounseokO@users.noreply.github.com>
2024-02-07 13:11:31 +0900

    Title: 연속부분수열일까, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240207/연속부분수열일까/contiguous-array-or-not.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString();
+
+input = input.split(/\n/);
+
+let a = input[1].split(/\s/);
+let b = input[2].split(/\s/);
+let answ = "No";
+
+for(var i = 0; i < a.length; i++){
+    if(a[i]== b[0]){
+        for(var x = 1; x < b.length; x++){
+            if(a[i+x]==b[x]){
+                answ = "Yes"
+            }else{
+                answ = "No"
+                break;
+            }
+        }
+    }
+}
+
+console.log(answ);
