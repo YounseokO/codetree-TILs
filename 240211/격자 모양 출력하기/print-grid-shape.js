231802d304ef95d5d3b98cb3c4640dd356a6fe89
YounseokO <104410211+YounseokO@users.noreply.github.com>
2024-02-11 23:47:43 +0900

    Title: 격자 모양 출력하기, Time: 230ms, Memory: 14MB, Status: Passed - CodeTree

--- 240211/격자 모양 출력하기/print-grid-shape.js
+++ 240211/격자 모양 출력하기/print-grid-shape.js
@@ -3,13 +3,14 @@ let input = fs.readFileSync(0).toString().trim().split(/\n/);
 
 let n = Number(input[0].trim().split(/\s/)[0]);
 let m = Number(input[0].trim().split(/\s/)[1]);
+
 let Arr = Array.from(Array(n), () => Array(n).fill(0));
 let tempData = "";
 
 let a = 0;
 let b = 0;
 
-for(var i = 1; i < m; i++){
+for(var i = 1; i <= m; i++){
     tempData = input[i].trim().split(/\s/);
     a = Number(tempData[0]);
     b = Number(tempData[1]);
