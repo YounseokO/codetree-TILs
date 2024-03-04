3611fd4168519b9143eb4b9dde95143b9b009e1a
YounseokO <104410211+YounseokO@users.noreply.github.com>
2024-03-04 13:08:49 +0900

    Title: 규칙에 따라 밀기, Time: 253ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240304/규칙에 따라 밀기/push-by-the-rules.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(/\n/);
+
+let word = input[0].split('');
+let man = input[1];
+
+let tempword = "";
+
+for(var i = 0; i < man.length; i++){
+    if(man[i] == "L"){
+        tempword = word.shift();
+        word.push(tempword); 
+    }else{
+        tempword = word.pop();
+        word = [tempword,...word]//tempword + word; 
+    }
+}
+console.log(word.join(''));
